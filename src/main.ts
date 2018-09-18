60de3f6bf9263e6e4582ae45980009f22301886a
DESKTOP-4E7GB0R\Peaksource <khaled@peaksource.tn>
2018-09-18 17:36:26 +0100

    clean main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// import * as $ from 'jquery' ;
 import * as $ from 'jquery';
-// import * as Waypoint from '../node_modules/waypoints/src/waypoint.js';
 import * as Waypoint from 'waypoints';
 import * as Plyr from 'plyr';
 
