13d68fdf8cf827851f36c455ed3afd4bf160ab2e
Khaled Weslati <weskhaled@gmail.com>
2018-10-10 06:15:24 +0100

    back to dev heroku

--- src/app/@core/env.ts
+++ src/app/@core/env.ts
@@ -1,13 +1,13 @@
-// const BACK_URL = 'https://weslati-khaled.herokuapp.com/' ;
-const BACK_URL = 'http://127.0.0.1:8000/' ;
+const BACK_URL = 'https://weslati-khaled.herokuapp.com/' ;
+// const BACK_URL = 'http://127.0.0.1:8000/' ;
 const BACK_API = BACK_URL + 'api/' ;
 const V_API = BACK_API + 'v1/' ;
 export class ENV {  
-    // public static  client_id = 6;
-    // public static  client_secret = 'v9IB4luyWjebXwVDXjPjGkXav0NgWmGJpplkOYgP';
+    public static  client_id = 6;
+    public static  client_secret = 'v9IB4luyWjebXwVDXjPjGkXav0NgWmGJpplkOYgP';
     // local
-    public static  client_id = 2;
-    public static  client_secret = 'vzPE9HRppkub5v2h928psbm6NBcOLpLgeFXiscg7';
+    // public static  client_id = 2;
+    // public static  client_secret = 'vzPE9HRppkub5v2h928psbm6NBcOLpLgeFXiscg7';
     
     public static  URL_TOKEN = BACK_URL + 'oauth/token';
     public static  URL_GET_USER = V_API + 'user';
